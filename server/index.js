31ac0a6f653c3ca22fa34f420e871e829ff02e00
Reinier van Limpt <reinier.van.limpt@gmail.com>
2018-03-13 02:33:17 +0100

    got stuck on adding a delete request (check lines 21 & 61)

--- server/index.js
+++ server/index.js
@@ -3,21 +3,24 @@
 var express = require('express')
 var db = require('../db')
 var helpers = require('./helpers')
+var slug = require('slug')  // replaces spacebars to be url friendly (copied from slides week 4)
+var bodyParser = require('body-parser')   // body-parser enables you to read the values of a form easily (copied from slides week 4)
 
 module.exports = express()
   .set('view engine', 'ejs') // configure express to use ejs for templating
   .set('views', 'view')
   .use(express.static('static'))
+  .use(bodyParser.urlencoded({extended: true})) //urlencoded is the was browsers normally send forms, (copied from slides week 4)
   // TODO: Serve the images in `db/image` on `/image`.
   .use('/image', express.static('db/image'))
-  .get('/', all)
-  .get('/:id', get) // function *get* will run when theres a GET request to an */:id*
   /* TODO: Other HTTP methods. */
   // .post('/', add)
   // .get('/:id', get)
   // .put('/:id', set)
   // .patch('/:id', change)
-  // .delete('/:id', remove)
+  //  .delete('/:id', removeTarget) --- got stuck on trying to add a function called *removeTarget* t0 run after a DELETE request is added to * /:id* while i defined the function on line 62 the terminal says its undefined??
+  .get('/', all)
+  .get('/:id', get) // function *get* will run when theres a GET request to an */:id*
   .listen(1902)
 
 function all(req, res) {
@@ -55,11 +58,18 @@ function get(req, res) {
     ]
       res.render('error.ejs', Object.assign({}, result, helpers))
   }
+// function removeTarget(req, res) {}
 
   if (has) // if the requested id is present in the database...
   {
-    result.data = db.get(id) // sends the data of the id on which a get request is executed
-    res.render('detail.ejs', Object.assign({}, result, helpers)) // renders the detail.ejs file
+    var result = {errors: [], data: db.get(id)}
+    res.format
+    ({
+      json: () => res.json(result),
+      html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
+    })
+  //  result.data = db.get(id) // sends the data of the id on which a get request is executed
+  //  res.render('detail.ejs', Object.assign({}, result, helpers)) // renders the detail.ejs file
   }
   else // if the requested id is not present in the database...
   {
